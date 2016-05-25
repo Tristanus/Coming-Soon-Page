30e401117b05042ea6ef9f0b910e7c0ea3d723eb
Tristan de Jager <info@tristandejager.com>
2016-05-25 16:38:39 +0200

    Inital Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Static Capture</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Mr+Dafoe' rel='stylesheet' type='text/css'>
+		<link rel="icon" href="img/favicon.png" type="image/x-icon" />
+	</head>
+	<body>
+		<div class="wrap">
+			<div class="content">
+				<img src="img/logo.png">
+				<div class="logo">	
+					<a href="index.html"><h1>Static Capture</h1></a>
+				</div>
+				<p>Be ready, we are launching soon &#33;</p>
+				<p class="copy">&#169; 2016 Designed by<a href="http://tristandejager.com" target="_blank">&nbsp;Tristan de Jager</a>. </p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
